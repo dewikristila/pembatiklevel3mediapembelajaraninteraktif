704ef8149c1e9cdf40d4fa7236d11e2d12d906ac
dewikristila <dewikristila@gmail.com>
2024-09-28 21:35:40 +0800

    UPLOADMPI

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{98E99F14-2307-4043-9411-7CCF5019B683}'];loadHandler&&loadHandler(0, '<div id="spr0_391227e3"><div id="spr1_391227e3" class="kern slide"><img id="img0_391227e3" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_391227e3" class="kern slide"></div></div>', '{"s":[]}');})();
